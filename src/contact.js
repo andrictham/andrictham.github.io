846bceb219d7ed9d3f8b19eb3587dffb1ea5702a
Sprazzeus <sprazzeus@gmail.com>
2017-03-12 21:44:09 +0800

    Added formspree contact form to Contact page.

--- src/contact.js
+++ src/contact.js
@@ -6,9 +6,21 @@ class Contact extends Component{
   render() {
     let path = this.props.location.pathname
     let array = this.props.pageIntro
+    let formspreeURL= "//formspree.io/" + this.props.profile.email
+
     return (
       <div className="contact">
         {getPageIntro(array, path)}
+
+        <form id="contactform" method="POST" action={formspreeURL}>
+          <input type="text" name="name" placeholder="Your name" />
+          <input type="email" name="_replyto" placeholder="Your email address" />
+          <input type="hidden" name="_subject" value="Website contact" />
+          <textarea name="message" placeholder="Your message"></textarea>
+          <input type="text" name="_gotcha" style={{display: "none"}} />
+          <input type="submit" value="Send" />
+        </form>
+
       </div>
     )
   }
